76253252403e061acfdda7865b196136b45c6d3e
Josh Thompson <thompsonjoshd@gmail.com>
2017-05-02 15:51:20 -0600

    add relationships to transactions
    added validations and relationships to transactions class
    updated tests to use factories

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,11 @@
 class Transaction < ApplicationRecord
 
+  validates :invoice, presence: true
+  validates :credit_card_number, presence: true
+  validates :result, presence: true
+
+  belongs_to :invoice
+
   enum status: [failed: 0, success: 1]
+
 end
