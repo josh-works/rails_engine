697658834abfdb1865e05aa3a04e947299c09c93
Josh Thompson <thompsonjoshd@gmail.com>
2017-05-02 15:32:45 -0600

    add validations, associations
    customers, merchants now have tests (that pass) for:
    validations of all fields
    associations for many-to-many relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
+    has_many :invoices
+    has_many :merchants, through: :invoices
+
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord
 
-  
+  validates :name, presence: true
+
+  has_many :invoices
+  has_many :customers, through: :invoices
+
 end
